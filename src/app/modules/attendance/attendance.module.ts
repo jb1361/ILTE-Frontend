efebaae3c7d33e027f3b9777d4010c78bf27e151
jb1361 <justinbutler4@hotmail.com>
2018-10-26 23:07:20 -0400

    Create header bar

--- src/app/modules/attendance/attendance.module.ts
+++ src/app/modules/attendance/attendance.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ViewAttendanceComponent } from './component/view-attendance/view-attendance.component';
+import {UtilModule} from '../util/util.module';
+import {HeaderBarComponent} from '../layout/component/header-bar/header-bar.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    UtilModule
   ],
-  declarations: [ViewAttendanceComponent]
+  declarations: [ViewAttendanceComponent, HeaderBarComponent]
 })
 export class AttendanceModule { }
