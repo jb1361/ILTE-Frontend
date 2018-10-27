71444094d53cade6ae13cc07e8612c6e70cd2f12
jb1361 <justinbutler4@hotmail.com>
2018-10-26 22:25:31 -0400

    Create header-bar component

---
+++ src/app/modules/layout/layout.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderBarComponent } from './component/header-bar/header-bar.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [HeaderBarComponent]
+})
+export class LayoutModule { }
